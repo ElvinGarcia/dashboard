7442334ac650a75f488f6970c4971cde91613216
Elvin <Elvin@ElvinGarcia.com>
2022-05-31 22:57:04 -0400

    inital structure layout

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createStore, applyMiddleware}  from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from "./reducers/rootReducer";
+import thunk from 'redux-thunk';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React>
-    <App />
-  </React>
+ReactDOM.render(
+  <Provider store={store}>
+  <App />
+  </Provider>,
+  document.getElementById('root')
 );
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+
+
+export default function rootReducer(state = {}, action){
+
+}
+
+
+
