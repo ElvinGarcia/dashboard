1fcf9bfe56f4493ae3587fc2dd2cb1880fb81d9d
Elvin <Elvin@ElvinGarcia.com>
2022-06-02 22:40:16 -0400

    index.js initial configured with all new required syntax for both reduxjs and reactjs

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import React from "react"
 
 const Home = () => {
   return (
-    <p>testing</p>
+    <p>testing testing testing testing</p>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import {configureStore, applyMiddleware} from 'redux'
+import { createRoot } from 'react-dom/client'; // replaces ReactDOM.render
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 import rootReducer from "./reducers/rootReducer";
-import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 
-// import { Provider } from 'react-redux'
-
-const store = configureStore(rootReducer, applyMiddleware(thunk));
+// The slice reducers were automatically passed to combineReducers()
+// The redux-thunk middleware was automatically added
+// Dev-mode middleware was added to catch accidental mutations
+// The Redux DevTools Extension was automatically set up
+// The middleware and DevTools enhancers were composed together and added to the store
+const store = configureStore({reducer:rootReducer});
 
 // returns an object with a render function
 const root = createRoot(document.querySelector('#root'));
-root.render(<App />)
+root.render(
+
+<Provider store={store}>
+  <App />
+</Provider>
+  )
+
 
 
 
