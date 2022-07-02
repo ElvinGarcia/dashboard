42547314bc803730f9420c9a0c44031bfbaa6c73
Elvin <Elvin@ElvinGarcia.com>
2022-07-02 14:29:02 -0400

    added an addition form component as a dropdown menu to each link indicating to either leave a comment or login to leave a comment

--- frontend/src/components/feedComponent/rss.js
+++ frontend/src/components/feedComponent/rss.js
@@ -1,10 +1,20 @@
 import React, {Component} from "react";
-
+import FeedMenu from '../menu/feedMenu'
 
 class Rss extends Component{
   render() {
     if(Array.isArray(this.props.feeds)){
-      return <ul className="col-sm-4">{this.props.feeds.map(obj =><li key={obj.id}><a href={obj.url} target="_blank" rel="noreferrer" > {obj.title}</a></li>)}</ul>
+      return (
+        <ul className="col-sm-4">{this.props.feeds.map(obj =>
+        <li key={obj.id}>
+          <a href={obj.url} target="_blank" rel="noreferrer" >
+            {obj.title}
+          </a>
+          {< FeedMenu/>}
+          </li>
+      )
+       }
+      </ul>)
     } else {
       return <li key={this.props.id}><a href={this.props.url} target="_blank" rel="noreferrer" > {this.props.title}</a></li>
       }
@@ -15,3 +25,5 @@ class Rss extends Component{
 }
 
 export default Rss;
+
+
