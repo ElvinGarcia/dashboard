d5b709aac72c0ea36685600b6cefbbabe5a537f1
Elvin <Elvin@ElvinGarcia.com>
2022-07-17 10:35:49 -0400

    added email validation at the user model level

--- backend/dashboard_api/v1/app/models/user.rb
+++ backend/dashboard_api/v1/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   attr_accessor :password
   has_secure_password
   validates :username, uniqueness: {case_sensitive: false}
+  validates :email, uniqueness: {case_sensitive: false}, format: { with: URI::MailTo::EMAIL_REGEXP } #this his method does not check domain
   has_many :comments, class_name: "comments", foreign_key: "reference_id", dependent: :destroy
   has_many :links, through: :comments
 
